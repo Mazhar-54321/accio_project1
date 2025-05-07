1f5e29eb0dec7b66830533291980fa22706cbcc0
Mazhar-54321 <syedmazharali742@gmail.com>
2025-05-07 19:23:09 +0530

    edited script.js file

--- script.js
+++ script.js
@@ -8,20 +8,46 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+ arr.filter((el)=>el.profession==="developer").map((el)=>{
+    console.log(`id:${el.id},Name:${el.name},Age:${el.age},Profession:${el.profession}`)
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((el)=>{
+    if(el.profession === "developer"){
+      console.log(`id:${el.id},Name:${el.name},Age:${el.age},Profession:${el.profession}`)
+    }
+  })
 }
 
 function addData() {
+  let obj={id:4, name:"susan", age:"20", profession:"intern"} 
+  arr.push(obj);
+  arr.map((el)=>{
+    console.log(`id:${el.id},Name:${el.name},Age:${el.age},Profession:${el.profession}`)
+  })
   //Write your code here, just console.log
 }
 
 function removeAdmin() {
+  arr =arr.filter((el)=>el.profession!=="admin")
+  arr.map((el)=>{
+    console.log(`id:${el.id},Name:${el.name},Age:${el.age},Profession:${el.profession}`)
+  })
   //Write your code here, just console.log
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 4, name: "john", age: "18", profession: "developer" },
+    { id: 5, name: "jack", age: "20", profession: "developer" },
+    { id: 6, name: "karen", age: "19", profession: "admin" },
+  ];
+  arr=arr.concat(arr1);
+  arr.map((el)=>{
+    console.log(`id:${el.id},Name:${el.name},Age:${el.age},Profession:${el.profession}`)
+  })
 }
